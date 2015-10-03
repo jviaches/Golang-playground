e9077a603c707d38893ea58cd0be39cae3672882
jviaches <jviaches@gmail.com>
2015-10-03 22:38:44 +0300

    Added another iterative fibonachi function that return function

--- Fibbonachi/fibbon.go
+++ Fibbonachi/fibbon.go
@@ -6,6 +6,11 @@ import "math"
 func main() {
 	fmt.Println(fibbRecc(6))
 	fmt.Println(fibShortIterative(6))
+
+	f:=fibIterFunc()
+	for i := 0; i < 10; i++ {
+		fmt.Print(f() )
+	}
 }
 
 func fibbRecc(numIndex int) (numValue int){
@@ -16,9 +21,20 @@ func fibbRecc(numIndex int) (numValue int){
 	return fibbRecc(numIndex - 2) + fibbRecc(numIndex -1)
 }
 
+// Short iterative mathematics form
 func fibShortIterative(numIndex float64) (float64){
 	Phi := (1 + math.Sqrt(5)) / 2
 	phi := (1 - math.Sqrt(5)) / 2
 
 	return (math.Pow(Phi, numIndex) - math.Pow(phi, numIndex)) / math.Sqrt(5)
+}
+
+func fibIterFunc() func() int{
+	x:=0
+	y:=1
+
+	return func() int{
+		x,y = y,x+y
+		return  x
+	}
 }
